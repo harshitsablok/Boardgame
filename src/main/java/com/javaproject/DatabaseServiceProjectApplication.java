85338d60c3bee8338c3a8c9dea42fdaf6eddfcda
Harshit Sablok <149866184+harshitsablok@users.noreply.github.com>
2026-07-12 10:27:12 +0530

    Update DatabaseServiceProjectApplication.java

--- src/main/java/com/javaproject/DatabaseServiceProjectApplication.java
+++ src/main/java/com/javaproject/DatabaseServiceProjectApplication.java
@@ -3,7 +3,7 @@ package com.javaproject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication 
 public class DatabaseServiceProjectApplication {
 
 	public static void main(String[] args) {
